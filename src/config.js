da75a698eab8da9a6f01a201cf2fb1ee2813bfb2
tomlebl <tl12@st-andrews.ac.uk>
2022-01-25 22:58:53 +0000

    uploader finished

--- src/config.js
+++ src/config.js
@@ -4,86 +4,92 @@ const prompt = require('prompt')
 const chalk = require('chalk')
 
 const readConfig = () => {
-	const configPath = fs.existsSync('./src/config/config.json')
-		? './src/config/config.json'
-		: './src/config/config-default.json'
-	const configJSON = fs.readFileSync(configPath).toString()
-	try {
-		return JSON.parse(configJSON)
-	} catch (err) {
-		console.log(chalk.red('ERROR - config.json is empty or corrupted'))
-		console.log(chalk.red.italic('Use app.js config to save new configuration'))
-		return {}
-	}
+  const configPath = fs.existsSync('./src/config/config.json')
+    ? './src/config/config.json'
+    : './src/config/config-default.json'
+  const configJSON = fs.readFileSync(configPath).toString()
+  try {
+    return JSON.parse(configJSON)
+  } catch (err) {
+    console.log(chalk.red('ERROR - config.json is empty or corrupted'))
+    console.log(chalk.red.italic('Use app.js config to save new configuration'))
+    return {}
+  }
 }
 
 const setConfig = list => {
-	// console.log(list)
-	if (list) {
-		console.log(chalk.greenBright.inverse(' *** Current client config *** '))
-		console.log(readConfig())
-	} else {
-		console.log(
-			chalk.cyanBright(
-				'This utility will walk you through client configuration stored in config.json file.\nPress enter to confirm current value shown in ()'
-			)
-		)
-		const configObj = readConfig()
-		prompt.start()
-		prompt.get(
-			[
-				{
-					name: 'instrumentId',
-					description: chalk.greenBright('Instrument ID found in Admin Setting Instruments table'),
-					type: 'string',
-					default: configObj.instrumentId
-				},
-				{
-					name: 'statusPath',
-					description: chalk.greenBright('Relative path to file with status table'),
-					type: 'string',
-					default: configObj.statusPath
-				},
-				{
-					name: 'historyPath',
-					description: chalk.greenBright('Relative path to file with history table'),
-					type: 'string',
-					default: configObj.historyPath
-				},
-				{
-					name: 'serverAddress',
-					description: chalk.greenBright('HTTP address of the server'),
-					type: 'string',
-					default: configObj.serverAddress
-				},
-				{
-					name: 'submissionPath',
-					description: chalk.greenBright('Relative to submission folder'),
-					type: 'string',
-					default: configObj.submissionPath
-				}
-			],
-			(err, result) => {
-				if (err) {
-					return onError(err)
-				}
-				const newConfigJSON = JSON.stringify(result)
-				fs.writeFile('./src/config/config.json', newConfigJSON, () => {
-					console.log(chalk.blue('     !!!   SUCCESS   !!!   '))
-					console.log(chalk.greenBright.inverse(' *** Current client config *** '))
-					console.log(readConfig())
-				})
-			}
-		)
-	}
+  // console.log(list)
+  if (list) {
+    console.log(chalk.greenBright.inverse(' *** Current client config *** '))
+    console.log(readConfig())
+  } else {
+    console.log(
+      chalk.cyanBright(
+        'This utility will walk you through client configuration stored in config.json file.\nPress enter to confirm current value shown in ()'
+      )
+    )
+    const configObj = readConfig()
+    prompt.start()
+    prompt.get(
+      [
+        {
+          name: 'instrumentId',
+          description: chalk.greenBright('Instrument ID found in Admin Setting Instruments table'),
+          type: 'string',
+          default: configObj.instrumentId
+        },
+        {
+          name: 'statusPath',
+          description: chalk.greenBright('Relative path to file with status table'),
+          type: 'string',
+          default: configObj.statusPath
+        },
+        {
+          name: 'historyPath',
+          description: chalk.greenBright('Relative path to file with history table'),
+          type: 'string',
+          default: configObj.historyPath
+        },
+        {
+          name: 'serverAddress',
+          description: chalk.greenBright('HTTP address of the server'),
+          type: 'string',
+          default: configObj.serverAddress
+        },
+        {
+          name: 'submissionPath',
+          description: chalk.greenBright('Relative to submission folder'),
+          type: 'string',
+          default: configObj.submissionPath
+        },
+        {
+          name: 'nmrDataPath',
+          description: chalk.greenBright('Absolute path to IconNMR data folder'),
+          type: 'string',
+          default: configObj.nmrDataPath
+        }
+      ],
+      (err, result) => {
+        if (err) {
+          return onError(err)
+        }
+        const newConfigJSON = JSON.stringify(result)
+        fs.writeFile('./src/config/config.json', newConfigJSON, () => {
+          console.log(chalk.blue('     !!!   SUCCESS   !!!   '))
+          console.log(chalk.greenBright.inverse(' *** Current client config *** '))
+          console.log(readConfig())
+        })
+      }
+    )
+  }
 }
 
 const onError = err => {
-	console.log(err)
-	return 1
+  console.log(err)
+  return 1
 }
 
 module.exports = {
-	readConfig: readConfig,
-	setConfig: setConfig
+  readConfig: readConfig,
+  setConfig: setConfig
 }
--- src/uploader.js
+++ src/uploader.js
@@ -1,13 +1,51 @@
+const path = require('path')
+const fs = require('fs/promises')
+
 const chalk = require('chalk')
+const FormData = require('form-data')
+const axios = require('axios')
+const ZipDir = require('zip-dir')
+
+const { readConfig } = require('./config')
+const { from } = require('form-data')
+const { Console } = require('console')
+const { instrumentId, nmrDataPath, serverAddress } = readConfig()
+
+if (process.env.NODE_ENV !== 'dev') {
+  serverAddress += '/api'
+}
 
 const uploader = (socket, verbose) => {
-  socket.on('upload', metadata => {
-    const { datasetName, expNo } = JSON.parse(metadata)
+  socket.on('upload', async payload => {
+    const { datasetName, expNo, group } = JSON.parse(payload)
     if (verbose) {
       console.log(
         chalk.magenta(`Uploading data ${datasetName}-${expNo}`),
         chalk.yellow(` [${new Date().toLocaleString()}]`)
       )
+    }
+
+    try {
+      console.time('upload')
+      const dataPath = path.join(nmrDataPath, group, 'nmr', datasetName, expNo)
+      const blob = await ZipDir(dataPath)
+
+      const form = new FormData()
+      form.append('instrumentId', instrumentId)
+      form.append('datasetName', datasetName)
+      form.append('expNo', expNo)
+      form.append('group', group)
+      form.append('nmrData', blob, 'nmrData.zip')
+
+      const response = await axios.post(serverAddress + '/data/auto', form, {
+        headers: { ...form.getHeaders() }
+      })
+      if (response.status === 200 && verbose) {
+        console.timeEnd('upload')
+        console.log(chalk.green('Success!'))
+      }
+    } catch (error) {
+      console.log(error)
     }
   })
 }
